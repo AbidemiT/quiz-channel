1c33fb801b72eec92b8debe0ee69da4dc29082fa
AbidemiT <tiamiyusikiruabidemi@gmail.com>
2023-01-21 08:46:44 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Questions from "./components/Questions.vue";
+import Result from "./components/Result.vue";
+</script>
+
+<template>
+  <div class="ctr">
+    <questions />
+    <result />
+
+    <button type="button" class="reset-btn">Reset</button>
+</div>
+</template>
+
+<style scoped>
+
+</style>
