b9114c730f37961b551e82b9b18767df5ed35afa
AbidemiT <tiamiyusikiruabidemi@gmail.com>
2023-01-21 10:26:43 +0100

    quiz channel

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,22 +1,28 @@
-<script>
+<script setup>
+const props = defineProps(['questions', 'questionsAnswered']);
+const emit = defineEmits(["question-answered"]);
+
+const selectAnswer = function (is_correct) {
+    emit("question-answered", is_correct);
+}
 
 </script>
 
 <template>
-  <div class="questions-ctr">
+    <div class="questions-ctr">
         <div class="progress">
-            <div class="bar"></div>
-            <div class="status">1 out of 3 questions answered</div>
+            <div class="bar" :style="`width: ${(questionsAnswered / questions.length) * 100}%`"></div>
+            <div class="status">{{questionsAnswered}} out of {{questions.length}} questions answered</div>
         </div>
-        <div class="single-question">
-            <div class="question">Sample Question 1</div>
+        <transition-group name="fade">
+            <div class="single-question" v-for="(question, qi) in questions" :key="question.q"
+            v-show="questionsAnswered === qi">
+            <div class="question">{{ question.q }}</div>
             <div class="answers">
-                <div class="answer">Sample Answer 1</div>
-                <div class="answer">Sample Answer 2</div>
-                <div class="answer">Sample Answer 3</div>
-                <div class="answer">Sample Answer 4</div>
+                <div class="answer" v-for="answer in question.answers" :key="answer.text" @click.prevent="selectAnswer(answer.is_correct)">{{ answer.text }}</div>
             </div>
         </div>
+        </transition-group>
     </div>
 </template>
 
--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,11 +1,25 @@
-<script>
+<script setup>
+import { computed } from "vue";
+const props = defineProps(['results', 'totalCorrect']);
+
+const resultIndex = computed(() => {
+    let index = 0;
+
+    props.results.forEach((e, i) => {
+        if (e.min <= props.totalCorrect && e.max >= props.totalCorrect) {
+            index = i;
+        }
+    });
+
+    return index;
+})
 </script>
 
 <template>
     <div class="result">
-        <div class="title">You got sample result 1!</div>
+        <div class="title">{{results[resultIndex].title}}</div>
         <div class="desc">
-            Enter a short description here about the result.
+            {{results[resultIndex].desc}}
         </div>
     </div>
 </template>
